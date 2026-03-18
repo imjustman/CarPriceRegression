be1c7b15887c5738c5d63fdbd416802853951d69
imjustman <newclgns4010@gmail.com>
2026-03-18 16:02:59 +0900

    #32[feat] explain.py코드 수정(캐시 기능 추가)

--- App/explain.py
+++ App/explain.py
@@ -1,11 +1,16 @@
+import streamlit as st
 import shap
+
+@st.cache_resource
+def get_explainer(_model):
+    return shap.TreeExplainer(_model)
 
 def compute_shap_values(pipeline, input_df):
     X_transformed = pipeline.named_steps['preprocess'].transform(input_df)
 
     model = pipeline.named_steps['model']
 
-    explainer = shap.TreeExplainer(model)
+    explainer = get_explainer(model)
     shap_values = explainer(X_transformed)[0].values.tolist()
     feature_labels = pipeline.named_steps['preprocess'].feature_names_in_.tolist()
 
